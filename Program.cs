0dd82fcf575e4d7e162ed6a46935d1ff64199654
Mario Martinez <martineznatali126@gmail.com>
2026-02-04 18:46:38 -0600

    potencia, raiz cuadrada y logaritmo Agregados

--- Program.cs
+++ Program.cs
@@ -8,19 +8,21 @@ namespace CalculadoraPOO
         {
             Calculadora calc = new Calculadora();
 
-            Console.WriteLine("=== CALCULADORA POO ===");
+            Console.WriteLine("=== CALCULADORA ===");
 
             Console.Write("Ingresa el primer número: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
-
             Console.Write("Ingresa el segundo número: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("\nElige una opción:");
-            Console.WriteLine("1. Sumar");
-            Console.WriteLine("2. Restar");
-            Console.WriteLine("3. Multiplicar");
-            Console.WriteLine("4. Dividir");
+            Console.WriteLine("1. SUMA");
+            Console.WriteLine("2. RESTA");
+            Console.WriteLine("3. MULTIPLICACION");
+            Console.WriteLine("4. DIVISION");
+            Console.WriteLine("5. POTENCIA");
+            Console.WriteLine("6. RAIZ CUADRADA (del primer número)");
+            Console.WriteLine("7. LOGARITMO (del primer número)");
             Console.Write("Opción: ");
 
             int opcion = Convert.ToInt32(Console.ReadLine());
@@ -42,18 +44,36 @@ namespace CalculadoraPOO
                     case 4:
                         resultado = calc.Dividir(num1, num2);
                         break;
+                    case 5:
+                        resultado = Math.Pow(num1, num2);
+                        break;
+                    case 6:
+                        resultado = Math.Sqrt(num1);
+                        break;
+                    case 7:
+                        resultado = Math.Log(num1);
+                        break;
                     default:
                         Console.WriteLine("Opción no válida");
                         return;
                 }
 
                 Console.WriteLine($"\nResultado: {resultado}");
+                Console.WriteLine("desea realizar otra operacion? (s/n)");
+                string respuesta = Console.ReadLine();
+                if (respuesta.ToLower() == "s")
+                {
+                    Main(args);
+                }
+                else if(respuesta.ToLower() == "n")
+                {
+                    Console.WriteLine("Gracias por usar la calculadora. ¡Hasta luego!");
+                }
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine($"Error: {e.Message}");
+                Console.WriteLine($"Error: {ex.Message}");
             }
-
             Console.ReadKey();
         }
     }
